d96263242a86d0f644c454796b14b23681206a0e
yeojin <d02jyjeong@naver.com>
2023-09-13 21:38:55 +0900

    [initial] 자료구조 1주차 과제

---
+++ c_ds/week1/recursion1/recursion1.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int fact(int n) {
+	if (n == 0)
+		return 1;
+	else
+		return n * fact(n - 1);
+}
+int main(void) {
+	int n;
+	printf("Enter a number:");
+	scanf("%d", &n);
+
+	printf("%d! = ", n);
+	printf("%d\n", fact(n));
+}
