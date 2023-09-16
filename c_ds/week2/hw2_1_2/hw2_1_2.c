b54a2e5ce1951f50e5100f20fcda225c9b140d6d
yeojin <d02jyjeong@naver.com>
2023-09-16 17:44:30 +0900

    [initial] hw2_1_2

---
+++ c_ds/week2/hw2_1_2/hw2_1_2.c
@@ -0,0 +1,111 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> // 문자열 처리를 위해 추가
+
+#define MAX_STACK_SIZE 3
+#define MAX_STRING 100
+
+typedef int element;
+
+typedef struct {
+    element data[MAX_STACK_SIZE];
+    char name[MAX_STACK_SIZE][MAX_STRING]; // 수정된 부분
+    int top;
+} StackType;
+
+void init_stack(StackType* s) {
+    s->top = -1;
+}
+
+int is_empty(StackType* s) {
+    return (s->top == -1);
+}
+
+int is_full(StackType* s) {
+    return (s->top == (MAX_STACK_SIZE - 1));
+}
+
+void push(StackType* s, element item, const char* name) { // 수정된 부분
+    if (is_full(s)) {
+        fprintf(stderr, "스택 포화 에러\n");
+        return;
+    }
+    else {
+        s->data[++(s->top)] = item;
+        strcpy(s->name[s->top], name); // 문자열 복사
+    }
+}
+
+element pop(StackType* s) {
+    if (is_empty(s)) {
+        fprintf(stderr, "스택 공백 에러\n");
+        exit(1);
+    }
+    else {
+        s->top--; // 데이터를 제거할 때는 값을 그냥 감소시키면 됨
+    }
+    return s->data[s->top + 1];
+}
+
+element peek(StackType* s) {
+    if (is_empty(s)) {
+        fprintf(stderr, "스택 공백 에러\n");
+        exit(1);
+    }
+    return s->data[s->top];
+}
+
+void stack_print(StackType* s) {
+    if (is_empty(s)) {
+        printf("<empty>\n--\n");
+        return;
+    }
+    else {
+        for (int i = s->top; i >= 0; i--) {
+            if (i == s->top) {
+                printf("[%d %s] <- top\n", s->data[i], s->name[i]);
+            }
+            else
+                printf("%d %s\n", s->data[i], s->name[i]);
+        }
+        printf("--\n");
+    }
+    return;
+}
+
+int main(void) {
+    StackType s; // 스택을 정적으로 생성
+
+    init_stack(&s);
+    stack_print(&s);
+
+    push(&s, 10, "ten");
+    stack_print(&s);
+
+    push(&s, 20, "twenty");
+    stack_print(&s);
+
+    push(&s, 30, "thirty");
+    stack_print(&s);
+
+    push(&s, 40, "fourty");
+    stack_print(&s);
+
+    pop(&s);
+    stack_print(&s);
+
+    push(&s, 50, "fifty");
+    stack_print(&s);
+
+    pop(&s);
+    stack_print(&s);
+
+    pop(&s);
+    stack_print(&s);
+
+    pop(&s);
+    stack_print(&s);
+
+    return 0;
+}
