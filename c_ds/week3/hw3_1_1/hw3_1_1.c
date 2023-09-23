bc994ff16e3ee23ddb8056a6957c59b224bb5f4d
yeojin <d02jyjeong@naver.com>
2023-09-24 00:01:14 +0900

    [update] 2)) 완료

--- c_ds/week3/hw3_1_1/hw3_1_1.c
+++ c_ds/week3/hw3_1_1/hw3_1_1.c
@@ -1,16 +1,21 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 #define MAX_QUEUE_SIZE 3
 
-typedef char element;
-typedef struct {
-	element men[MAX_QUEUE_SIZE];
-	element women[MAX_QUEUE_SIZE];
+// typedef char element;
 
-	int front_men, rear_men;
-	int front_women, rear_women;
+typedef struct {
+	char name[10];
+	char gender[2];
+}element;
+
+typedef struct {
+	element man[MAX_QUEUE_SIZE];
+	int front;
+	int rear;
 }QueueType;
 
 void error(char* message) {
@@ -18,27 +23,61 @@ void error(char* message) {
 	exit(1);
 }
 
-void init_queue(QueueType* pm, QueueType* pwm) {
-	pm->front_men = pm->rear_men = 0;
-	pwm->front_women = pwm->rear_women = 0;
+void init_queue(QueueType* p) {
+	p->front = -1;
+	p->rear = -1;
 }
 
-int is_empty(QueueType* pm, QueueType* pwm) {
-	if (pm->front_men == pm->rear_men || pwm->front_women == pwm->rear_women)
+int is_empty(QueueType* p) {
+	if (p->front == p->rear)
 		return 1;
 	else
 		return 0;
 }
 
-int is_full(QueueType* pm, QueueType* pwm) {
-	if ((pm->rear_men + 1) % MAX_QUEUE_SIZE == pm->front_men || (pwm->rear_women + 1) % MAX_QUEUE_SIZE == pwm->front_women)
+int is_full(QueueType* p) {
+	if ((p->rear + 1) % MAX_QUEUE_SIZE == p->front)
 		return 1;
 	else
 		return 0;
 }
+
+void enqueue(QueueType* p, element item) {
+	if (is_full(p)) {
+		error("큐가 포화상태입니다.");
+		return;
+	}
+	p->man[++(p->rear)] = item;
+}
+
+element dequeue(QueueType* p) {
+	if (is_empty(p)) {
+		error("큐가 공백상태입니다.");
+	}
+	element item = p->man[++(p->front)];
+	return item;
+}
+
+int get_count(QueueType *p) {
+	return p->rear - p->front;
+}
+
+void print_queue(QueueType* p) {
+	printf("%s", p->man->name);
+}
+
 int main(void) {
 
+	QueueType manQ;
+	element newPerson;
 
+	init_queue(&manQ);
+
+	printf("이름을 입력:");
+	scanf("%s", newPerson.name);
+	enqueue(&manQ, newPerson);
+	printf("%d명: ", get_count(&manQ));
+	print_queue(&manQ);
 
 	return 0;
 }
