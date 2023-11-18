408ba01ecb2c2966853d9f15cac96aac351650e1
yeojin <d02jyjeong@naver.com>
2023-11-18 23:37:38 +0900

    [update] read_graph, write_graph 수정

--- c_ds/week11/hw6_1_1/hw6_1_1.c
+++ c_ds/week11/hw6_1_1/hw6_1_1.c
@@ -1,6 +1,7 @@
 // 그래프 추상 타입의 구현(인접 행렬 이용)
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
 #include "queue.h"
 #define TRUE 1
 #define FALSE 0
@@ -17,9 +18,9 @@ void graph_init(GraphType* g) {
 		for (c = 0; c < MAX_VERTICES; c++)
 			g->adj_mat[r][c] = 0;
 }
-void insert_edge(GraphType *g, int start, int end) { // 간선 삽입
+void insert_edge(GraphType* g, int start, int end) { // 간선 삽입
 	if (start >= g->n || end >= g->n) {
-		fprintf(stderr, "그래프 정점 번호 오류"); 
+		fprintf(stderr, "그래프 정점 번호 오류");
 		return;
 	}
 	g->adj_mat[start][end] = 1;
@@ -33,7 +34,7 @@ void delete_edge(GraphType* g, int start, int end) { // 간선 삭제
 	g->adj_mat[start][end] = 0;
 	g->adj_mat[end][start] = 0;
 }
-void insert_vertex(GraphType *g) { // 정점 삽입
+void insert_vertex(GraphType* g) { // 정점 삽입
 	if ((g->n) + 1 > MAX_VERTICES) {
 		fprintf(stderr, "그래프 정점의 개수 초과");
 		return;
@@ -49,7 +50,7 @@ void dfs_mat(GraphType* g, int v) {
 			dfs_mat(g, w);
 }
 void bfs_mat(GraphType* g, int v) {
-	int w; 
+	int w;
 	QueueType q;
 	init(&q);
 	visited[v] = TRUE;
@@ -57,7 +58,7 @@ void bfs_mat(GraphType* g, int v) {
 	enqueue(&q, v);
 	while (!is_empty(&q)) {
 		v = dequeue(&q);
-		for(w = 0; w < g->n; w++)
+		for (w = 0; w < g->n; w++)
 			if (g->adj_mat[v][w] && !visited[w]) {
 				visited[w] = TRUE;
 				printf("%d", w);
@@ -77,15 +78,12 @@ void read_graph(GraphType* g, char* filename) {
 		fprintf(stderr, "파일 %s를 열 수 없음\n", filename);
 		return;
 	}
-
 	fscanf(fp, "%d", &numOfVer);
 	g->n = numOfVer;
 
 	while (fscanf(fp, "%d %d", &start, &end) != EOF) {
 		insert_edge(g, start, end);
-		insert_vertex(g);
 	}
-
 	fclose(fp);
 }
 void write_graph(GraphType* g, char* filename) {
@@ -100,25 +98,25 @@ void write_graph(GraphType* g, char* filename) {
 			return;
 		}
 	}
-
 	for (int i = 0; i < g->n; i++) {
 		for (int j = 0; j < g->n; j++)
 			fprintf(fp, "%d", g->adj_mat[i][j]);
 		fprintf(fp, "\n");
 	}
-
 	fclose(fp);
 }
-
 void print_adj_mat(GraphType* g) {
-	for (int i = 0; i < g->n; i++)
-		for (int j = 0; j < g->n; j++)
-			printf("%2d ", g->adj_mat[i][j]);
-	printf("\n");
+	for (int i = 0; i < g->n; i++) {
+		for (int j = 0; j < g->n; j++) {
+			printf("%d", g->adj_mat[i][j]);
+		}
+		printf("\n");
+	}
 }
 int main(void) {
 	GraphType g;
-	
+	graph_init(&g);
+
 	read_graph(&g, "input.txt");
 	write_graph(&g, "output.txt");
 
