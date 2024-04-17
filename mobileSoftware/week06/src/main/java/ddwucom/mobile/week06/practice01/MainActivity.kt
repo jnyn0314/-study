f58a855eefa49e53ae7440973bde6409e7f036be
Yeojin0523 <d02jyjeong@naver.com>
2024-04-17 13:26:12 +0900

    [update] 모소 6주차 과제

--- mobileSoftware/week06/src/main/java/ddwucom/mobile/week06/practice01/MainActivity.kt
+++ mobileSoftware/week06/src/main/java/ddwucom/mobile/week06/practice01/MainActivity.kt
@@ -1,5 +1,6 @@
 package ddwucom.mobile.week06.practice01
 
+import android.app.Activity
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -18,16 +19,46 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(mainBinding.root)
+
+        // 3. SAM
+        mainBinding.btnOutput.setOnClickListener{
+            val myEdit : EditText = mainBinding.etInput
+            val myTextView : TextView = mainBinding.tvDisplay
+
+            myTextView.text = myEdit.text
+        }
+
+        /*
+        // 2. object 객체 생성
+        val myClick = object : View.OnClickListener{
+            override fun onClick(view : View?){
+                val myEdit : EditText = mainBinding.etInput
+
+                val myTextView : TextView = mainBinding.tvDisplay
+                myTextView.text = myEdit.text
+            }
+        }
+
+        mainBinding.btnOutput.setOnClickListener(myClick)
+        */
+
+
+        /* 1. 리스너 인터페이스 작성
+        val myClick = MyClick(this)
+        mainBinding.btnOutput.setOnClickListener(myClick) */
+
     }
 
-    class MyClick(context: Context) : View.OnClickListener{
+    // 1. 리스너 인터페이스 작성
+    /*
+    class MyClick(val activity: MainActivity) : View.OnClickListener{
         override fun onClick(v: View?) {
             // textView에 출력해라.
-            val myEdit : EditText = mainBinding.etInput
-            val text = myEdit.text
+            val myEdit : EditText = activity.mainBinding.etInput
 
-            val textView : TextView = mainBinding.tvDisplay
-            textView.text = myEdit.text
+            val textView : TextView = activity.mainBinding.tvDisplay
+            textView.text = myEdit.text // 값 읽어서 text에 출력
         }
     }
+    */
 }
